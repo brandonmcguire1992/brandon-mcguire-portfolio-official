3c30f9416948b9b8721fb8b04cef3cd03dc0bcfb
Brandon McGuire <brandonmcguire1992@gmail.com>
2020-10-04 19:48:48 -0500

    Updates

---
+++ precache-manifest.5081c221b644cbcb262923e7bb01c9e4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ef6f66efc8da19289b8a4ad15e89dc",
+    "url": "/brandon-mcguire-portfolio/index.html"
+  },
+  {
+    "revision": "e2cea011cb341aa9ce0a",
+    "url": "/brandon-mcguire-portfolio/static/css/main.d311e06a.chunk.css"
+  },
+  {
+    "revision": "c32631dd69b7fd239746",
+    "url": "/brandon-mcguire-portfolio/static/js/2.de82c3fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brandon-mcguire-portfolio/static/js/2.de82c3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2cea011cb341aa9ce0a",
+    "url": "/brandon-mcguire-portfolio/static/js/main.0cbd1a5c.chunk.js"
+  },
+  {
+    "revision": "97583e44fa17caf84787",
+    "url": "/brandon-mcguire-portfolio/static/js/runtime-main.ac3b9e48.js"
+  },
+  {
+    "revision": "ba78aab949243a555c4fa051dee56e3c",
+    "url": "/brandon-mcguire-portfolio/static/media/Barkeep.ba78aab9.jpg"
+  },
+  {
+    "revision": "eb9c7d5a304a4957d28ad8c793e0cb68",
+    "url": "/brandon-mcguire-portfolio/static/media/Horiseon.eb9c7d5a.jpg"
+  },
+  {
+    "revision": "5a43c1560ed19400c1df401112d57f3b",
+    "url": "/brandon-mcguire-portfolio/static/media/Note Taker.5a43c156.jpg"
+  },
+  {
+    "revision": "1f39d869d1e7a5ab8a18523c37644743",
+    "url": "/brandon-mcguire-portfolio/static/media/Run Buddy.1f39d869.jpg"
+  },
+  {
+    "revision": "0350903809f1f7a64db5936c923bc14f",
+    "url": "/brandon-mcguire-portfolio/static/media/Simple Social.03509038.jpg"
+  },
+  {
+    "revision": "3a793acce650fe0c8bc2afb5e65bec71",
+    "url": "/brandon-mcguire-portfolio/static/media/brandon-portfolio.3a793acc.jpg"
+  },
+  {
+    "revision": "2756b20c2952beb3050b878c0319f639",
+    "url": "/brandon-mcguire-portfolio/static/media/coding.2756b20c.jpg"
+  },
+  {
+    "revision": "3fce925663b8e722cbd773b9f27d2fe3",
+    "url": "/brandon-mcguire-portfolio/static/media/full-stack-developer-banner.3fce9256.jpg"
+  },
+  {
+    "revision": "e0ef73e7a18ac7be23498cb94088449f",
+    "url": "/brandon-mcguire-portfolio/static/media/portfolio.e0ef73e7.jpg"
+  },
+  {
+    "revision": "a434b31b9bf0afbb7072895b40348203",
+    "url": "/brandon-mcguire-portfolio/static/media/profile.a434b31b.jpg"
+  },
+  {
+    "revision": "9ab088cf2e39f37c667293b4d26d7bff",
+    "url": "/brandon-mcguire-portfolio/static/media/weather-dashboard.9ab088cf.jpg"
+  },
+  {
+    "revision": "81ce12115280539ff8a68f031dbe3766",
+    "url": "/brandon-mcguire-portfolio/static/media/work-day-scheduler.81ce1211.jpg"
+  }
+]);
